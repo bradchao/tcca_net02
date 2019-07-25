2ffe1e1c044ee1aa5549f2b0ab148702abce959c
Brad <brad@brad.tw>
2019-07-25 19:50:15 +0800

    2019072502

--- app/src/main/java/tw/org/tcca/app/netstatustest/MainActivity.java
+++ app/src/main/java/tw/org/tcca/app/netstatustest/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     private ConnectivityManager cmgr;
@@ -16,5 +18,15 @@ public class MainActivity extends AppCompatActivity {
 
         cmgr = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
 
+        Log.v("brad", "network:" + isConnectNetwork());
+
     }
+
+    private boolean isConnectNetwork(){
+        NetworkInfo info = cmgr.getActiveNetworkInfo();
+        boolean isConnected = info != null && info.isConnectedOrConnecting();
+        return  isConnected;
+
+    }
+
 }
